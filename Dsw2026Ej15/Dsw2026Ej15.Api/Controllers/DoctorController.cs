ec94cdcba190acfab2772764adb01ea8a0af7156
Nazarena Martinez <nazarenamartinez91@gmail.com>
2026-06-19 00:07:46 -0300

    Resolusión hasta el primer endpoint

---
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorController.cs
@@ -0,0 +1,40 @@
+﻿using Dsw2026Ej15.Api.Models;
+using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Controllers;
+
+[ApiController]
+[Route("api/doctors")]//nombre de ruta del endpoint
+public class DoctorController : ControllerBase
+{
+    //inyectamos 
+    private readonly IPersistence _persistence;
+    public DoctorController(IPersistence persistence)
+    {
+        _persistence = persistence;
+    }
+    [HttpPost]//verbo
+    public async Task<ActionResult> CreateDoctor([FromBody]DoctorModel.Request request)//si no tengo nada definido en el metodo, cuando ejecuto viene vacio
+    {
+        //metodo para la logica para validaciones
+        if(string.IsNullOrWhiteSpace(request.Name) ||
+                string.IsNullOrWhiteSpace(request.LicenseNumber))
+        {
+            return BadRequest("Nombre y matricula son requeridos");
+        }
+
+        var speciality = _persistence.GetSpecialityById(request.SpecialityId);
+
+        if (speciality == null)
+        { 
+            return BadRequest("Especialidad no existe");
+        }
+
+        var doctor = new Doctor(request.Name, request.LicenseNumber, speciality);
+        _persistence.SaveDoctor(doctor);
+            return Ok(); 
+    }
+}}
--- Dsw2026Ej15/Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Dsw2026Ej15.Data;
+using Dsw2026Ej15.Domain.Interfaces;
+
 namespace Dsw2026Ej15.Api
 {
     public class Program
@@ -10,15 +13,16 @@ namespace Dsw2026Ej15.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseAuthorization();
