795ea38096d6c7a132e3ebccc084ab9e0ee9aa42
Nicolas Perry <nicolasperry04@gmail.com>
2026-07-02 21:04:52 -0300

    realizo los cambios necesarios para preparar la implementacion  de una base de datos

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Doctor> Doctors { get; set; }
+
+    public DbSet<Speciality> Specialities { get; set; }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceEf.cs
@@ -0,0 +1,38 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class PersistenceEf : IPersistence
+{
+    private readonly AppDbContext _context;
+
+    public PersistenceEf(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Doctor> GetAllDoctors()
+    {
+        var doctors = _context.Doctors.ToList();
+        return doctors;
+    }
+
+    public Doctor? GetDoctorById(Guid id)
+    {
+        return _context.Doctors.SingleOrDefault(d => d.Id == id);
+    }
+
+    public Speciality? GetSpecialityById(Guid id)
+    {
+        Speciality? speciality =_context.Specialities.FirstOrDefault(s => s.Id == id);
+        return speciality;
+    }
+
+    public void SaveDoctor(Doctor doctor)
+    {
+        _context.Doctors.Add(doctor);
+        _context.SaveChanges();
+    }
+}
